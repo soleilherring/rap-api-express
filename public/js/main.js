57d154352f50ee149e2e7cdfef05c92e44895681
Soleil <soleilherring@gmail.com>
2025-05-27 16:42:44 +0100

    fix?

--- public/js/main.js
+++ public/js/main.js
@@ -2,13 +2,14 @@ document.querySelector('button').addEventListener('click', getName)
 alert('okay')
 async function getName(){
     const rapperName = document.querySelector('input').value
-    try{
-        const response = await fetch(`https://rap-api-express-v8w2.vercel.app/api/${rapperName}`)
-        const data = await response.json()
-        console.log(data)
-        document.querySelector('h2').textContent =`Their birthname: ${data.birthname}`
-    }
-    catch (err) {
-        console.log(err)
-    }
+    console.log(rapperName)
+    // try{
+        // const response = await fetch(`https://rap-api-express-v8w2.vercel.app/api/${rapperName}`)
+    //     const data = await response.json()
+    //     console.log(data)
+    //     document.querySelector('h2').textContent =`Their birthname: ${data.birthname}`
+    // }
+    // catch (err) {
+    //     console.log(err)
+    // }
 }
