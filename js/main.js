295d0605f7e7755a0e48e78ab6d38ca4822c06a6
Soleil <soleilherring@gmail.com>
2025-05-27 15:10:10 +0100

    modifications

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-alert('working')
+document.querySelector('button').addEventListener('click', getName)
+alert('okay')
+// function getName(){
+//     document.querySelector('h2').textContent = 
+// }
