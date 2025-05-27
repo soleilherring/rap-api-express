77c881327768570ecd78cb3bee63252f092aa240
Soleil <soleilherring@gmail.com>
2025-05-27 15:31:03 +0100

    added on lcick

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
-// document.querySelector('button').addEventListener('click', getName)
-alert('okay')
-// function getName(){
-//     document.querySelector('h2').textContent = 
-// }
+document.querySelector('button').addEventListener('click', getName)
+// alert('okay')
+async function getName(){
+    const rapperName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://rap-api-express-v8w2.vercel.app/api/${rapperName}`)
+        const data = await response.json()
+        document.querySelector('h2').textContent =data.rapperName
+    }
+    catch (err) {
+        console.error.log(err)
+    }
+    document.querySelector('h2').textContent = 
+}
